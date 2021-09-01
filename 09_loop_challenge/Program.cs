42b99967ddef6b6c8b6f412642f1cd770bf62aa3
amyjprogrammer <amyjprogrammer@gmail.com>
2021-09-01 19:19:07 -0400

    The fizz buzz challenge

---
+++ 09_loop_challenge/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_loop_challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int num = 525;
+            
+            for(int i = 500; i <= num; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            int num2 = 100;
+            for (int i = 0; i <= num2; i += 5)
+            {
+                Console.WriteLine(i);
+            }
+            
+
+            int fizz = 100;
+            for (int i = 1; i <= fizz; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+        }
+    }
+}
---
+++ 09_loops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+
+            while(number <= 100)
+            {
+                Console.WriteLine(number); //print 0 to 100 skipping everything 5th
+                number += 5;
+            }
+
+            Console.ReadLine();
+
+            int number1 = 100;
+
+            for (int i = 1; i <= number1; i++)
+            {
+                Console.WriteLine(i); //prints 1 to 100
+            }
+        }
+    }
+}
