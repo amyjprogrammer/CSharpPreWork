4bbf3d94951f4e5eda5618be16d360485b337cb2
amyjprogrammer <amyjprogrammer@gmail.com>
2021-09-01 19:57:10 -0400

    Classes and Objects

---
+++ 10_classes_and_objects/Donut.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_classes_and_objects
+{
+    class Donut
+    {
+        //properties will go here in the next module
+        public string Type { get; set; } //shortcut prop tab twice
+        public string Filling { get; set; } //remember the shortcut prop tab twice
+        public decimal Price { get; set; }//prop tab twice
+        public bool IsSpecial { get; set; }
+        public bool Sprinkles { get; set; }
+
+        public bool isRound { get; set; }
+
+    }
+}
---
+++ 10_classes_and_objects/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_classes_and_objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiating the objects
+            Donut dougDonut = new Donut();
+            Donut chrisDonut = new Donut();
+            Donut nickDonut = new Donut();
+
+            //setting the property values
+            dougDonut.Filling = "cherry";
+            dougDonut.Price = 3;
+            dougDonut.Type = "normal";
+            dougDonut.IsSpecial = true;
+            dougDonut.Sprinkles = false;
+            dougDonut.isRound = true;
+
+            chrisDonut.Filling = "none";
+            chrisDonut.Price = 2;
+            chrisDonut.Type = "small";
+            chrisDonut.IsSpecial = false;
+            chrisDonut.Sprinkles = false;
+            chrisDonut.isRound = false;
+
+            nickDonut.Filling = "chocolate";
+            nickDonut.Price = 3;
+            nickDonut.Type = "normal";
+            nickDonut.IsSpecial = true;
+            nickDonut.Sprinkles = true;
+            nickDonut.isRound = true;
+
+            Console.WriteLine(nickDonut.Filling);
+        }
+    }
+}
